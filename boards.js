f47e40eb196c1905c7ed71c569543b609319e919
Patricio Whittingslow <graded.sp@gmail.com>
2026-05-27 12:18:02 -0300

    remove c3 since it has no LED

--- boards.js
+++ boards.js
@@ -237,12 +237,6 @@ const boards = {
 		compiler: 'tinygo',
 		code: exampleBlinky1,
 	},
-	'xiao-esp32c3': {
-		humanName: 'Seeed XIAO ESP32-C3',
-		location: 'parts/xiao-esp32c3.json',
-		compiler: 'tinygo',
-		code: exampleBlinky1,
-	},
 	'xiao-esp32s3': {
 		humanName: 'Seeed XIAO ESP32-S3',
 		location: 'parts/xiao-esp32s3.json',
